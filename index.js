8a48af8bdc8262e2a564182ef02b89e1a72dea20
Mert Duyar <86599460+thejaxen@users.noreply.github.com>
2024-01-07 18:57:51 +0300

    07.01.2024
    Rendering the tags is done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-ReactDOM.render(<p>First project with React and .NET</p>, document.getElementById("root"));
+//ReactDOM.render(<p>First project with React and .NET</p>, document.getElementById("root"));
+
+/*ReactDOM.render(
+    <div>
+        <h1>React Course</h1>
+        <p>Building a project with .NET and React</p>
+        </div>,
+        document.getElementById("root")
+    );
+*/
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <div>
+        <h1>Reacte Course</h1>
+        <p>Building a project with .NET and React keeps on.</p>
+    </div>
+);
