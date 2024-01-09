e0af8f27d6678b42202d4a6600bb525a23db61f9
Mert Duyar <86599460+thejaxen@users.noreply.github.com>
2024-01-09 23:44:02 +0300

    09.01.2024
    Using funtions in rendering.

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -4,12 +4,25 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-  
+  <React.StrictMode> 
+  <div>
+    <MainBody/>
+  </div>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+function MainBody(){
+  return (
+    <div> 
+      <p>Using functions in rendering.</p>
+    </div>
+  );
+}
+
+
+
+
+
+//REACT-STRICTMODE SHOWS US THE POSİİBLE ERRORS AND EXCEPTİONs.
+
 
