4eb2b6db09ebdb8567a2ddba017d3b7ff31d287a
Mert Duyar <86599460+thejaxen@users.noreply.github.com>
2024-01-14 01:19:11 +0300

    10.01.2024
    Usage of src,img and avatar.

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -17,10 +17,19 @@ root.render(
 
 function Students(){
   const fullName = "Kris Walley";
-  co
+  const programingExp = 2;
   return (
     <div className="container p-4">
       <div className="row">Students enrolled.</div>
+      <div className="row border">
+        <div className="col-1">
+          <img className="w-100" src={'https://ui-avatars.com/api/?name=${fullName}'}></img>
+        </div>
+        <div className="col-10">
+          {fullName}<br></br>
+          Programming Experience {programingExp} years
+        </div>
+      </div>
     </div>
   );
 }
