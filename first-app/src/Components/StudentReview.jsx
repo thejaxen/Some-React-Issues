95e2f0bb87ac99e109659dfb880e4b816bd560eb
Mert Duyar <86599460+thejaxen@users.noreply.github.com>
2024-01-31 23:35:34 +0300

    31.01.2024
    Turning functional components into class components

--- first-app/src/Components/StudentReview.jsx
+++ first-app/src/Components/StudentReview.jsx
@@ -1,9 +1,13 @@
-const StudentReview = () =>{
+import React from "react";
+
+class StudentReview extends React.Component{
+    render(){
     return(
         <div className="p-2">
             <i class="bi bi-hand-thumbs-up-fill text-success p-1" ></i> 
             <i class="bi bi-hand-thumbs-down-fill text-danger p-1"></i>
         </div>
-    )
+    );
+    }
 }
 export default StudentReview;
