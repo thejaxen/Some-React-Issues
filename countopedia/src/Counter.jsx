d3a53c2370e8231c4d6d898ba2cd7d8e6caaa538
Mert Duyar <86599460+thejaxen@users.noreply.github.com>
2024-02-05 23:41:19 +0300

    05.02.2024
    Putting buttons for counter.

--- countopedia/src/Counter.jsx
+++ countopedia/src/Counter.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 class Counter extends React.Component{
     render(){
         return(
-            <div>Counter</div>
+            <div className="row">
+                <h1>
+                    Counter:
+                </h1>
+                <button style={{width:"200px"}}>+1</button>
+                <p></p>
+                <button style={{width:"200px"}}>-1</button>
+            </div>
         );
     }
 }
