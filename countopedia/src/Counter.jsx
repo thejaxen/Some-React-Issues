a08f8f605b8ff775c6eb374339ee42e06701a98a
Mert Duyar <86599460+thejaxen@users.noreply.github.com>
2024-02-07 02:49:29 +0300

    07.02.2024
    SetState usage done.

--- countopedia/src/Counter.jsx
+++ countopedia/src/Counter.jsx
@@ -3,15 +3,19 @@ import React from 'react';
 class Counter extends React.Component{
     constructor(props){
         super(props);
+        this.handleAttack = this.handleAttack.bind(this);
+        this.handleDefence=this.handleDefence.bind(this);
         this.state = {
             count:0,
         };
     }
     handleAttack(){
-        alert("Attack clicked");
+        
+        this.setState({count: this.state.count + 1});
     }
     handleDefence(){
-        alert("Defend clicked");
+        
+        this.setState({count: this.state.count - 1});
     }
     render(){
         return(
