0e84ee6dc9b3e848b78f4fea56157fa5efbd6aa8
Mert Duyar <86599460+thejaxen@users.noreply.github.com>
2024-02-06 23:16:33 +0300

    Button Props
    What happens when you click on buttons.

--- countopedia/src/Counter.jsx
+++ countopedia/src/Counter.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
 class Counter extends React.Component{
+    handleAttack(){
+        alert("Attack clicked");
+    }
+    defendAttack(){
+        alert("Defend clicked");
+    }
     render(){
         return(
             <div className="row">
                 <h1>
                     Counter:
                 </h1>
-                <button style={{width:"200px"}}>+1</button>
+                <button onClick={this.handleAttack} style={{width:"200px"}}>+1</button>
                 <p></p>
-                <button style={{width:"200px"}}>-1</button>
+                <button onClicik={this.defendAttack} style={{width:"200px"}}>-1</button>
             </div>
         );
     }
