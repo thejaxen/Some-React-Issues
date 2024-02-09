fcc587d551e1193156035d580f3df484b8eec387
Mert Duyar <86599460+thejaxen@users.noreply.github.com>
2024-02-09 19:17:32 +0300

    09.02.2024
    Handling attacks

--- countopedia/src/Counter.jsx
+++ countopedia/src/Counter.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import attack from "./attack.png";
+import defend from "./defend.png";
 
 class Counter extends React.Component{
     constructor(props){
@@ -10,22 +12,37 @@ class Counter extends React.Component{
         };
     }
     handleAttack(){
-        
-        this.setState({count: this.state.count + 1});
+        this.setState((previousState) => {
+            return{
+                count: previousState.count + 1,
+            };
+        });
     }
     handleDefence(){
-        
-        this.setState({count: this.state.count - 1});
+     this.setState((previousState) => {
+        return{count: this.state.count - 1,};
+     });   
     }
+    
     render(){
         return(
-            <div className="row">
+            <div className="row text-center">
                 <h1>
-                    Counter:{this.state.count}
+                    Game Score:{this.state.count}
                 </h1>
+                <p>You win at +20 points and lose at -20 points</p>
+                <p>Last Play :</p>
+                <h3>Game Status :</h3>
+                <div className="col-6 col-md-3 offset-md-3"> 
+                <img style ={{
+                    width:"100px",
+                    cursor:"pointer",
+                    border: "1px solid green",}}  className="p-4 rounded" src={attack} onClick={this.handleAttack}
+                ></img>
+                </div>
                 <button onClick={this.handleAttack} style={{width:"200px"}}>+1</button>
                 <p></p>
-                <button onClick={this.handleDefence} style={{width:"200px"}}>-1</button>
+                <button onClick={this.handleDefence} style={{width:"200px"}}>-1</button>    
             </div>
         );
     }
