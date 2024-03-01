6c1af4ef1e88b4f88f752e785a5ae1f92b83617c
Mert Duyar <86599460+thejaxen@users.noreply.github.com>
2024-03-01 23:46:48 +0300

    01.03.2024
    New Project Contactopedia started.

---
+++ contactopedia/contactopedia/src/Header.jsx
@@ -0,0 +1,22 @@
+
+import logo from "./logo192.png";
+
+const Header = () => {
+    return(
+        <div>
+            <MainHeader/>
+            
+        </div>
+    );
+}
+function MainHeader(){
+    return(
+        <div>
+            
+            <span style={{backgroundColor:"white",color:"cyan",fontWeight:"bold",fontSize:"20px",textAlign:"center"}}>
+            <img src={logo} style={{height:"40px",verticalAlign:"top"}}></img>
+                React Course - Contactopedia</span>
+        </div>
+    );
+}
+export default Header;
